524212820f795522f4889088acf45f16f0683d7a
fedeesti <fede.estigarribia@hotmail.com>
2022-03-01 15:46:06 -0300

    Add project skeleton
    All the dependencies to use are added, the data folder next to its equipment files and eslint/prettier is configured.

---
+++ src/client/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="text-center min-h-screen">
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      black: 'black',
+      ghostwhite: 'ghostwhite',
+      goldenrod: 'goldenrod',
+      'body-linear-first': '#09203f',
+      'body-linear-second': '#537895',
+      'btn-linear-first': '#f5b814',
+      'btn-linear-second': '#ed8317',
+      'link-color': '#0000ee',
+      'ligth-green': '#c9e4ca',
+      'dark-green': '#a1c181',
+      required: '#f94144',
+      'charcoal-light': '#364958',
+      'charcoal-dark': '#233d4d',
+    },
+  },
+  plugins: [],
+};
